ca5697779c8f9d396f8d0248f7df448cd80d5cab
Shridhar Sundarraj <shridhar_3@hotmail.com>
2016-01-09 18:48:02 -0500

    Run in terminal, input (1)

--- definitions.rkt
+++ definitions.rkt
@@ -9,8 +9,8 @@
 (provide public-key)
 (provide private-key)
 
-(define min-range (expt 10 3))
-(define max-range (expt 10 5))
+(define min-range (expt 10 25))
+(define max-range (expt 10 30))
 
 (define p (find-prime -1 min-range max-range))
 (define q (find-prime p min-range max-range))
@@ -36,7 +36,9 @@
 (define public-key (list e n))
 (define private-key (list d n))
 
-(define (encrypt message [public-e e] [n1 n])
+(define (encrypt message [public-key (list e n)])
+  (define public-e (first public-key))
+  (define n1 (second public-key))
   (local[(define (main-func message)
            (cond[(empty? message) empty]
                 [else (cons (en/decrypt (char->integer (first message)) public-e n1)
@@ -44,10 +46,11 @@
     (main-func (string->list message))))
 
 
-(define (decrypt message [private-d d] [n1 n])
+(define (decrypt message [private-key (list d n)])
+  (define private-d (first private-key))
+  (define n1 (second private-key))
   (local [(define (main-func message)
             (cond[(empty? message) empty]
                  [else (cons (integer->char (en/decrypt (first message) private-d n1))
                              (main-func (rest message)))]))]
     (list->string (main-func message))))
-
