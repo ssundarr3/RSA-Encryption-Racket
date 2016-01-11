4e69c18e776f245f7df39cb539aa306b14268275
Shridhar Sundarraj <shridhar_3@hotmail.com>
2016-01-10 19:44:29 -0500

    Adding Documentation (1)

--- find-two-primes.rkt
+++ find-two-primes.rkt
@@ -1,11 +1,18 @@
 #lang racket
 
-(provide find-prime)
+;; A module for finding two distinct primes [description of module]
+(provide find-prime) ;; [list of functions]
 (require math/number-theory)
 
+;; (find-prime n min max) finds a prime that is in between min and max
+;; and is not equal to n.
+;; find-prime: Nat Nat Nat -> Nat
 
+
+;;;;;;;;;; IMPLEMENTATION ;;;;;;;;;;
+
+;; see interface above [no further info required]
 (define (find-prime p min max)
   (local[(define rp  (random-prime max))]
     (cond[(and (> rp min) (not (= p rp))) rp]
          [else (find-prime p min max)])))
-
