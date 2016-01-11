4dac3e1f5eb6f1f078c73d02f3afadf9cc8628f6
Shridhar Sundarraj <shridhar_3@hotmail.com>
2016-01-10 19:45:23 -0500

    Adding Documentation (1)

--- ran-num-gen.rkt
+++ ran-num-gen.rkt
@@ -1,11 +1,17 @@
 #lang racket
 
-(provide random-number)
 
-;; Finds a random integer in [min, max)
+;; A module for finding a random integer in [min, max) [description of module]
+(provide random-number) ;; [list of functions]
+
+;; (random-number min max) produces a random number in [min, max]
+;; random-number: Int Int -> Int
 ;; if min = max, it produces min.
+
+;;;;;;;;;; IMPLEMENTATION ;;;;;;;;;;
+
+;; see interface above [no further info required]
 (define (random-number min max)
   (floor (+ min (* (- max min) 
                    (/ (random 99999) 
                       99999)))))
-
